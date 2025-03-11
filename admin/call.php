1010adbb2fdb08c7407ded3bacad4c7288321d89
aydellast <ayudellaastuti23@gmail.com>
2025-03-11 14:38:46 +0700

    pertemuantiga

---
+++ admin/call.php
@@ -0,0 +1,20 @@
+<?php 
+    include_once 'top.php';
+?>
+<div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <?php 
+                    include_once 'menu.php';
+                ?>
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">File Praktikum</h1>
+
+                <?php
+                    include_once 'praktikum3/pesananpizza.html';
+                ?>
+                </div>
+</div>
+<?php 
+    include_once 'bottom.php';
+?>
